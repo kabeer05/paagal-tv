3f4ed2dd8ff5892ed505be9747609486d960a21b
Kabeer Arora <kabeerarora13@gmail.com>
2023-12-15 00:48:27 +0530

    [client]: Added custom colors in tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,7 +6,17 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        "chinese-black": "#151515",
+        "smoky-black": "#0E0E0E",
+        "vivid-red": "#F70D19",
+        "ku-crimson": "#E10013",
+        "harvard-crimson": "#C00016",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
